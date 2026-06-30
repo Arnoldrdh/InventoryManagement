ae91ed4c64acf76675fa63e4a0e602ede07f25f1
Arnoldrdh <arnoldrubben94@gmail.com>
2026-06-30 23:08:37 +0700

    add: Repositories Product

---
+++ InventoryManagement/Repositories/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using InventoryManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagement.Repositories
+{
+    public class ProductRepository
+    {
+        private List<Products> product = new List<Products>();
+            
+    }
+}
