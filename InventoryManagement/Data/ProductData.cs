ed6a7b2128884ff6a703c566fa8d2736babf28bc
Arnoldrdh <arnoldrubben94@gmail.com>
2026-07-02 00:36:00 +0700

    Feat: Added method mapping data

---
+++ InventoryManagement/Data/ProductData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace InventoryManagement.Data
+{
+    public class ProductData
+    {
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int ProductPrice { get; set; }
+
+        public int ProductStock { get; set; }
+
+        public string ProductType { get; set; }
+
+        public int? WarrantyMonth { get; set; }
+
+        public DateTime? ExpiredDate { get; set; }
+    }
+}
--- InventoryManagement/Repositories/ProductRepository.cs
+++ InventoryManagement/Repositories/ProductRepository.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
+using System.IO;
+using InventoryManagement.Data;
 
 namespace InventoryManagement.Repositories
 {
@@ -9,12 +12,101 @@ namespace InventoryManagement.Repositories
     {
         private const string FilePath = "product.json";
         private List<Products> products = new List<Products>();
+        
+        public ProductRepository()
+        {
+            LoadFromFile();
+        }
 
+        public Products MapToProduct(ProductData productData)
+        {
+            if (productData.ProductType == "Elektronic")
+            {
+                return new Elektronic(
+                    productData.ProductID,
+                    productData.ProductName,
+                    productData.ProductPrice,
+                    productData.ProductStock,
+                    productData.WarrantyMonth.Value
+                );
+            }
+            else if (productData.ProductType == "Food")
+            {
+                return new Food(
+                    productData.ProductID,
+                    productData.ProductName,
+                    productData.ProductPrice,
+                    productData.ProductStock,
+                    productData.ExpiredDate.Value
+                );
+            }
+
+            throw new Exception("Unknown Product Type");
+        }
+
+        public ProductData MapToProductData(Products product)
+        {
+            ProductData productData = new ProductData();
+
+            //Mapping
+            productData.ProductID = product.ProductId;
+            productData.ProductName = product.ProductName;
+            productData.ProductStock = product.ProductStock;
+            productData.ProductPrice = product.ProductPrice;
+            productData.ProductType = product.ProductType;
+
+            if(product is Elektronic elektronic)
+            {
+                productData.WarrantyMonth = elektronic.WarrantyMonth;
+            }
+
+            else if (product is Food food)
+            {
+                productData.ExpiredDate = food.ExpiredDate;
+            }
+
+           
+            return productData;
+
+        }
         private void SaveToFile()
         {
+            List<ProductData> productDataList = new List<ProductData>();
+            foreach (var product in products)
+            {
+                productDataList.Add(MapToProductData(product));
+            }
+
+            string json = JsonSerializer.Serialize(productDataList);
+            File.WriteAllText(FilePath, json);
 
         }
 
+        private void LoadFromFile()
+        {
+            if (!File.Exists(FilePath))
+            {
+                return;
+            }
+
+            string json = File.ReadAllText(FilePath);
+
+            List<ProductData>? productDatas =
+                JsonSerializer.Deserialize<List<ProductData>>(json);
+
+            if (productDatas == null)
+            {
+                return;
+            }
+
+            foreach (var productData in productDatas)
+            {
+                products.Add(MapToProduct(productData));
+            }
+        }
+
+
+        
         public void Add(Products product)
         {
             products.Add(product);
